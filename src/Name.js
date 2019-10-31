d79b9a024ca24f565f7591b0fe6e4b7eebcc9988
Constantine Chutis <constantinec84@gmail.com>
2019-10-31 17:08:50 -0400

    Reverted index.js back to default create app

---
+++ src/Name.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Name = () => {
+    return (
+        <div className="name">
+            <h1>CONSTANTINE CHUTIS</h1>
+        </div>
+    );
+}
+
+export default Name;
