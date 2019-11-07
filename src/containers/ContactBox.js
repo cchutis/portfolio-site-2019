e07a65c1db2621eeee04a384a375a51a633996d0
Constantine Chutis <constantinec84@gmail.com>
2019-11-07 14:25:19 -0500

    temp button styling

--- src/containers/ContactBox.js
+++ src/containers/ContactBox.js
@@ -3,7 +3,16 @@ import React from 'react';
 const ContactBox = () => {
     return (
         <div className="contact-box">
-            
+            <div className="contact-top-row">
+                <button className="contact-button">Github</button>
+                <button className="contact-button">Linkedin</button>
+                <button className="contact-button">Medium</button>
+            </div>
+            <div className="contact-bottom-row">
+                <button className="contact-button">Resume</button>
+                <button className="contact-button">Projects</button>
+                <button className="contact-button">Contact</button>
+            </div>
         </div>
     );
 }
