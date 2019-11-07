7d0271676ddef16b0deb029a6969eb0710fe61ab
Constantine Chutis <constantinec84@gmail.com>
2019-11-07 18:36:38 -0500

    Added slider component for project views

--- src/containers/PortfolioEntries.js
+++ src/containers/PortfolioEntries.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 export default class PortfolioEntries extends Component {
 
     renderProjects = () => {
         return this.props.projects.map(project => {
-            return <h3>{project.name}</h3>
+            return <Slide index={project.index - 1}><h2>{project.name}</h2></Slide>
         })
     }
 
     render() {
         return (
-            <div className="portfolio-box">
-                {this.renderProjects()}
+            <div className="">
+                <CarouselProvider
+                    naturalSlideWidth={100}
+                    naturalSlideHeight={100}
+                    totalSlides={9}>
+                    <ButtonBack className="back-button">Back</ButtonBack>
+                    <Slider>
+                        {this.renderProjects()}
+                    </Slider>
+                <ButtonNext className="next-button">Next</ButtonNext>
+                </CarouselProvider>
             </div>
         );
     }
