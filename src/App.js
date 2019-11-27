cc07d2e3683b7f203e2c4e201a13868445504c5c
Constantine Chutis <constantinec84@gmail.com>
2019-11-27 16:05:49 -0500

    Project picker function almost finished. done for day

--- src/App.js
+++ src/App.js
@@ -6,22 +6,13 @@ import MainContainer from './containers/MainContainer';
 const API = 'http://localhost:3000/projects';
 
 let currentID = 0;
-const colorArray = [
-  "green",
-  "red",
-  "blue",
-  "pink",
-  "orange",
-  "yellow",
-  "purple"
-];
 
 export default class App extends Component {
   
   state = {
     projects: [],
     selectedProject: {},
-    background: "green"
+    background: "white"
   }
 
   componentDidMount() {
@@ -39,14 +30,15 @@ export default class App extends Component {
   }
 
   projectPicker = (e) => {
-  //Right Arrow Picker
-  if(e.keyCode === 39) {
-    console.log("Right Arrow")
-    if(currentID >= 0 && currentID < this.state.projects.length) {
-      currentID++
-      this.setProjectParameters()  
+    //Right Arrow Picker
+    if(e.keyCode === 39) {
+      console.log("Right Arrow")
+      if(currentID >= 0 && currentID < this.state.projects.length) {
+        currentID++
+        this.setProjectParameters()  
+      }
     }
-  }
+    //Left Arrow Picker
     if(e.keyCode === 37) {
       console.log("Left Arrow")
       if(currentID <= this.state.projects.length) {
@@ -63,35 +55,6 @@ export default class App extends Component {
       background: this.state.selectedProject.color
     });
   }
-
-  // colorChanger = (e) => {
-    
-    
-  //   if(e.keyCode === 37) {
-  //     console.log("Left")
-  //     if(currentColor !== 0) {
-  //       currentColor--
-  //       this.setState({
-  //         background: colorArray[currentColor]
-  //       })
-  //     }
-  //   } else if(e.keyCode === 39) {
-  //     console.log("Right")
-  //     if(currentColor !== 6) {
-  //       currentColor++
-  //       this.setState({
-  //         background: colorArray[currentColor]
-  //       });
-  //     }
-  //   }
-  // }
-
-  // backgroundChange = (id) => {
-  //   const backgroundImg = this.state.projects.filter(project => id === project.id)
-  //   this.setState({
-  //     background: backgroundImg
-  //   })
-  // }
 
   render() {
     return (
